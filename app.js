560ee15b57cb2827ce4f192e5adc802d4c7797ab
Iris Coats <iris.coats73@gmail.com>
2024-01-23 12:58:36 -0600

    created email form

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit, (event") => {
     if (form.uname.value < 5){
-        alert("Username must be at least 5 characters long");
-        event.preventDefault();
+        alert("Username must be at least 5 characters long")
+        event.preventDefault()
     }
-}
+};
+
+// document.getElementById("uname").addEventListener("click", function(event){
+//     event.preventDefault()
+//   });
