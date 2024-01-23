e1c4802de004cff5a880aac7d90c9420b5beb1be
Iris Coats <iris.coats73@gmail.com>
2024-01-23 13:10:12 -0600

    working on assignment

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-use strict;
+"use strict";
 
 const form = document.querySelector("form");
 
-form.addEventListener("submit, (event") => {
-    if (form.uname.value < 5){
+form.addEventListener("submit, (event") = {
+    if (form.uname.value < 5) {
         alert("Username must be at least 5 characters long")
         event.preventDefault()
     }
 };
 
-// document.getElementById("uname").addEventListener("click", function(event){
-//     event.preventDefault()
-//   });
+// I can't get the event listener to work.  I want an alert stating user name needs to be at least 5 characters long
